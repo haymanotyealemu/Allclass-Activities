55da1a9778ec7b5545d3cbf47b3984ca976063c2
Haymanot W <haymanotalemu21@gmail.com>
2020-07-13 14:53:59 -0400

    addtodos

---
+++ Unit-4Web-API/25-Stu_Render-Todos/Unsolved/script.js
@@ -0,0 +1,16 @@
+var todoInput = document.querySelector("#todo-text");
+var todoForm = document.querySelector("#todo-form");
+var todoList = document.querySelector("#todo-list");
+var todoCountSpan = document.querySelector("#todo-count");
+
+var todos = ["Learn HTML", "Learn CSS", "Learn JavaScript"];
+function render(){
+    todoList.innerHTML = "";
+    for (var i=0; todos.length;i++){
+        
+
+        var newLi= document.createElement("li");
+        var content= newLi.textContent.todos[i];
+        todoList.appendChild(content);
+    }
+}
