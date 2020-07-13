bd9016a881865d541a2ebd78f246b824fffef3e8
Haymanot W <haymanotalemu21@gmail.com>
2020-07-13 16:01:24 -0400

    add todo list

---
+++ Unit-4Web-API/26-Stu_Add-Todos/Unsolved/script.js
@@ -0,0 +1,36 @@
+var todoInput = document.querySelector("#todo-text");
+var todoForm = document.querySelector("#todo-form");
+var todoList = document.querySelector("#todo-list");
+var todoCountSpan = document.querySelector("#todo-count");
+
+var todos = ["Learn HTML", "Learn CSS", "Learn JavaScript"];
+
+renderTodos();
+
+function renderTodos() {
+  // Clear todoList element and update todoCountSpan
+  todoList.innerHTML = "";
+  todoCountSpan.textContent = todos.length;
+
+  // Render a new li for each todo
+  for (var i = 0; i < todos.length; i++) {
+    var todo = todos[i];
+
+    var li = document.createElement("li");
+    li.textContent = todo;
+    todoList.appendChild(li);
+  }
+}
+function add(event){
+  event.prventDefault();
+  var newinput = todoInput.value.trim();
+  if(newinput === ""){
+    return;
+  
+  }
+  todos.push(newinput);
+  newinput.value = "";
+  renderTodos();
+  
+}
+todoForm.addEventListener("submit",add());
