2e5c46174e5850a19865044346f656f650a30abc
Haymanot W <haymanotalemu21@gmail.com>
2020-08-25 11:05:36 -0400

    ORM-To-Sequelize

--- Unit-14--Full-Stack/14.2-Activities/07-ORM-To-Sequelize/Unsolved/models/todo.js
+++ Unit-14--Full-Stack/14.2-Activities/07-ORM-To-Sequelize/Unsolved/models/todo.js
@@ -1,5 +1,5 @@
 module.exports = function(sequelize, DataTypes){
-    let Todo = sequelize.define("Todo", {
+    let Todo = sequelize.define("todos", {
         text: DataTypes.STRING,
         complete: DataTypes.BOOLEAN
     });
